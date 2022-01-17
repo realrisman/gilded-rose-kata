9bdfe82f393fee7dd2465411f7de7e238c4b5d09
Muhamad Risman <real.risman@gmail.com>
2022-01-17 21:59:46 +0700

    Add the solution

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -1,55 +1,94 @@
-function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
-}
+var GildedRose = function () {
+  var items = [];
+  items.push(new Item("+5 Dexterity Vest", 10, 20));
+  items.push(new Item("Aged Brie", 2, 0));
+  items.push(new Item("Elixir of the Mongoose", 5, 7));
+  items.push(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+  items.push(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+  items.push(new Item("Conjured Mana Cake", 3, 6));
+  GildedRose.updateQuality(items);
+};
 
-var items = []
-
-function update_quality() {
+GildedRose.updateQuality = function (items) {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
+    if (this.hasToDecreaseQuality(items[i])) {
+      this.decreaseQuality(items[i]);
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+
+    if (!this.hasToDecreaseQuality(items[i])) {
+      this.increaseDailyQuality(items[i]);
     }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
+
+    if (this.hasToDecreaseSellIn(items[i])) {
+      this.decreaseSellIn(items[i]);
+    }
+
+    if (this.hasExpired(items[i]) && this.perishWhenExpires(items[i])) {
+      this.runOutOfValue(items[i]);
+    }
+
+    if (this.hasExpired(items[i]) && !this.perishWhenExpires(items[i])) {
+      this.decreaseQuality(items[i]);
     }
   }
-}
+  return items;
+};
+
+GildedRose.hasToDecreaseQuality = function (item) {
+  return (
+    "Aged Brie" != item.name &&
+    "Backstage passes to a TAFKAL80ETC concert" != item.name &&
+    "Sulfuras, Hand of Ragnaros" != item.name
+  );
+};
+
+GildedRose.increaseDailyQuality = function (item) {
+  this.increaseQuality(item);
+
+  if (
+    ("Aged Brie" == item.name ||
+      "Backstage passes to a TAFKAL80ETC concert" == item.name) &&
+    item.sellIn < 11
+  ) {
+    this.increaseQuality(item);
+    if (item.sellIn < 6) {
+      this.increaseQuality(item);
+    }
+  }
+};
+
+GildedRose.increaseQuality = function (item) {
+  if (item.quality < 50) {
+    item.quality++;
+  }
+};
+
+GildedRose.decreaseQuality = function (item) {
+  if (item.quality > 0) {
+    item.quality--;
+  }
+};
+
+GildedRose.runOutOfValue = function (item) {
+  item.quality = 0;
+};
+
+GildedRose.hasToDecreaseSellIn = function (item) {
+  return "Sulfuras, Hand of Ragnaros" != item.name;
+};
+
+GildedRose.decreaseSellIn = function (item) {
+  item.sellIn--;
+};
+
+GildedRose.hasExpired = function (item) {
+  return item.sellIn < 0;
+};
+
+GildedRose.perishWhenExpires = function (item) {
+  return (
+    "Aged Brie" == item.name ||
+    "Backstage passes to a TAFKAL80ETC concert" == item.name ||
+    "Sulfuras, Hand of Ragnaros" == item.name
+  );
+};
